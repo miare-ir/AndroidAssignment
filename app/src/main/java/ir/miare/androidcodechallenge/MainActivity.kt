187985f5e2fe9dd2b573a5ce96183f0f52c5b3af
zahrarezaee <zr.evergreen2020@gmail.com>
2025-09-24 21:44:42 +0330

    Refactor project and remove unused files
    - Remove `RankingFragment`, `PlayerInfoBottomSheet`, and `models.kt`
    - Rename `MyApp` to `MyApplication` and update AndroidManifest
    - Remove fragment transaction from `MainActivity`

--- app/src/main/java/ir/miare/androidcodechallenge/MainActivity.kt
+++ app/src/main/java/ir/miare/androidcodechallenge/MainActivity.kt
@@ -12,9 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
-
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragmentContainer, RankingFragment(-1))
-            .commit()
     }
 }
--- app/src/main/java/ir/miare/androidcodechallenge/MyApplication.kt
+++ app/src/main/java/ir/miare/androidcodechallenge/MyApplication.kt
@@ -4,14 +4,14 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApp : Application() {
+class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         application = this
     }
 
-    companion object {
+    companion object Companion {
         lateinit var application: Application
     }
 }
