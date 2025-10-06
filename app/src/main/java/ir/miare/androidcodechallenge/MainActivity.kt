0816b0bf62fd77411c259372a8b74e1e9eefe9cc
morz <morz@gmail.com>
2025-10-06 20:55:53 +0330

    remove old files

--- app/src/main/java/ir/miare/androidcodechallenge/MainActivity.kt
+++ app/src/main/java/ir/miare/androidcodechallenge/MainActivity.kt
@@ -1,20 +1,42 @@
 package ir.miare.androidcodechallenge
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import ir.miare.androidcodechallenge.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
-class MainActivity : AppCompatActivity() {
-
-    var binding: ActivityMainBinding? = null
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
-
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragmentContainer, RankingFragment(-1))
-            .commit()
+        setContent { HelloScreen() }
     }
 }
+
+@Composable
+fun HelloScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        BasicText("Hello, World!")
+    }
+}
+
+@Preview(
+    name = "Hello Preview",
+    showBackground = true,
+    backgroundColor = 0xFFFFFFFF
+)
+@Composable
+fun HelloScreenPreview() {
+    HelloScreen()
+}
+
